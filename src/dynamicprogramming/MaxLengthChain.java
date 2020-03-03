21e14b945d28915ecf2845ff95995b0a6f8c8b83
vinit parmar <vinitparmar89@gmail.com>
2020-03-04 00:18:24 +0530

    longest chain algo

--- src/dynamicprogramming/MaxLengthChain.java
+++ src/dynamicprogramming/MaxLengthChain.java
@@ -10,8 +10,28 @@ public class MaxLengthChain {
 	}
 	
 	static int maxlength(MaxLengthChain[] arr , int n) {
+		int i,j,max=0;
+		int[] mcl = new int[n];
 		
-		return 0;
+		for(i=0;i<n;i++) {
+			mcl[i]=1;
+		}
+		
+		for(i=1;i<n;i++) {
+			for(j=0;j<i;j++) {
+				if(arr[i].a >arr[j].b && mcl[i] < mcl[j]+1) {
+					mcl[i] = mcl[j] + 1;
+				}
+			}
+		}
+		
+		for(i=0 ;i<n;i++) {
+			if(max<mcl[i]) {
+				max = mcl[i];
+			}
+		}
+		
+		return max;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
