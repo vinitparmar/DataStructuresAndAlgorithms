031cc4b72a031bdad4a72a9d3302ad39e5df3a24
vinit parmar <vinitparmar89@gmail.com>
2020-03-14 19:00:05 +0530

    added graph adjency matrix and adjency list

---
+++ src/graph/GraphAdjencyMatrix.java
@@ -0,0 +1,35 @@
+package graph;
+
+public class GraphAdjencyMatrix {
+
+	private boolean adjMatrix[][];
+	private int vertexCount;
+	
+	public GraphAdjencyMatrix(int vertexCount) {
+		this.vertexCount = vertexCount;
+		adjMatrix = new boolean [vertexCount][vertexCount];
+	}
+	
+	public void addEdge(int i, int j) {
+		if(i>=0 && i<vertexCount && j>=0 && j<vertexCount ) {
+			adjMatrix[i][j] = true;
+			adjMatrix[j][i] = true;
+		}
+	}
+	
+	public void removeEdge(int i, int j) {
+		if(i>=0 && i<vertexCount && j>=0 && j<vertexCount ) {
+			adjMatrix[i][j] = false;
+			adjMatrix[j][i] = false;
+		}
+	}
+	
+	public boolean isEdge(int i, int j) {
+		if(i>=0 && i<vertexCount && j>=0 && j<vertexCount ) {
+			return adjMatrix[i][j];
+		}else {
+			return false;
+		}
+	}
+
+}
---
+++ src/graph/ListNode.java
@@ -0,0 +1,9 @@
+package graph;
+
+public class ListNode {
+	int data;
+	private ListNode next;
+	public ListNode() {
+	}
+
+}
